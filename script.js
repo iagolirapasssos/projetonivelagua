7a340cd877761291022a05d3ef59ee3a07b4e43b
Iago lira <iagolirapassos@gmail.com>
2024-05-04 14:27:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.API_KEY_PLACEHOLDER,
+  authDomain: process.env.AUTH_DOMAIN_PLACEHOLDER,
+  databaseURL: process.env.DATABASE_URL_PLACEHOLDER,
+  projectId: process.env.PROJECT_ID_PLACEHOLDER,
+  storageBucket: process.env.STORAGE_BUCKET_PLACEHOLDER,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID_PLACEHOLDER,
+  appId: process.env.APP_ID_PLACEHOLDER,
+  measurementId: process.env.MEASUREMENT_ID_PLACEHOLDER
 };
-
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
