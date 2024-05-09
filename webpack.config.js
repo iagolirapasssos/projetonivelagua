39032c403754258ab6f2b1d07f566bba46c21400
iagolirapassos <iagolirapassos@gmail.com>
2024-05-08 22:29:19 -0300

    Correção nas variáveis secretas

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,16 @@
-const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/script.js',  // Certifique-se de que esse é o caminho correto para seu arquivo de entrada principal
-  output: {
-    path: path.resolve(__dirname, 'dist'),  // Este é o diretório onde os arquivos resultantes serão colocados
-    filename: 'bundle.js'  // O nome do arquivo de saída
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.API_KEY_PLACEHOLDER': JSON.stringify(process.env.API_KEY_PLACEHOLDER),
-      'process.env.APP_ID_PLACEHOLDER': JSON.stringify(process.env.APP_ID_PLACEHOLDER),
-      'process.env.AUTH_DOMAIN_PLACEHOLDER': JSON.stringify(process.env.AUTH_DOMAIN_PLACEHOLDER),
-      'process.env.DATABASE_URL_PLACEHOLDER': JSON.stringify(process.env.DATABASE_URL_PLACEHOLDER),
-      'process.env.MEASUREMENT_ID_PLACEHOLDER': JSON.stringify(process.env.MEASUREMENT_ID_PLACEHOLDER),
-      'process.env.MESSAGING_SENDER_ID_PLACEHOLDER': JSON.stringify(process.env.MESSAGING_SENDER_ID_PLACEHOLDER),
-      'process.env.PROJECT_ID_PLACEHOLDER': JSON.stringify(process.env.PROJECT_ID_PLACEHOLDER),
-      'process.env.STORAGE_BUCKET_PLACEHOLDER': JSON.stringify(process.env.STORAGE_BUCKET_PLACEHOLDER)
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+      'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+      'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+      'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+      'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+      'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+      'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID),
     })
-  ],
-  mode: 'production'
+  ]
 };
