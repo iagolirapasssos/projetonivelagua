b8d9020292d7d80d1c5e6de28a7e0d13f70d8d0d
iagolirapassos <iagolirapassos@gmail.com>
2024-05-08 22:41:06 -0300

    Correçãoem webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,26 @@
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  entry: './src/script.js',  // Certifique-se de que esse é o caminho correto para seu arquivo de entrada principal
+  output: {
+    path: path.resolve(__dirname, 'dist'),  // Este é o diretório onde os arquivos resultantes serão colocados
+    filename: 'bundle.js'  // O nome do arquivo de saída
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
@@ -10,7 +30,8 @@ module.exports = {
       'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
       'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
       'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
-      'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID),
+      'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID)
     })
-  ]
+  ],
+  mode: 'production'
 };
